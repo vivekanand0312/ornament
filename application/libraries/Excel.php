6602eaf3ce2978b2a869583bd7758b99d5819b44
unknown <Vivekanand@vivekanand.itv.com>
2017-02-23 15:44:01 +0530

    Well docummeted

--- application/libraries/Excel.php
+++ application/libraries/Excel.php
@@ -1,12 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /* 
  *  ======================================= 
- *  Author     : Muhammad Surya Ikhsanudin 
+ *  Author     : vivekanand
  *  License    : Protected 
- *  Email      : mutofiyah@gmail.com 
- *   
- *  Dilarang merubah, mengganti dan mendistribusikan 
- *  ulang tanpa sepengetahuan Author 
+ *  Email      : vivekanand0312@gmail.com 
  *  ======================================= 
  */  
 require_once APPPATH."/third_party/PHPExcel.php"; 
