098601377753c407ddf65568a2b454153507cc50
Vivekanand Sharma <vivekanand0312@gmail.com>
2017-02-23 14:08:04 +0530

    Update admin.php

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,53 +1,81 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
+/**
+ * Admin
+ * 
+ * @package   
+ * @author ornament
+ * @copyright Vivekanand
+ * @version 2016
+ * @access public
+ */
 class Admin extends CI_Controller {
-
+       /**
+        * Admin::__construct()
+        * 
+        * @return
+        */
        public function __construct() {
                parent::__construct();
                $this->load->model('Auth_model');
         }
-    
+       /**
+        * Admin::index()
+        * 
+        * @return
+        */
        public function index(){
         $this->sign_in();
        }
-        
+	
+       /**
+        * Admin::login()
+        * 
+        * @return
+        */
        public function login(){
            $this->form_validation->set_rules('username','Username','required');
            $this->form_validation->set_rules('password','Password','required');
             
             if ($this->form_validation->run() == FALSE){
-		  echo'<div class="alert alert-dismissable alert-danger"><small>'. validation_errors().'</small></div>';
-            }
-            else {
-                    $name = $this->input->post('username');
-                    $password = $this->input->post('password');
-                    if($this->Auth_model->auth($name, $password)){
-                    }
-                    else{
-                        echo'<div class="alert alert-dismissable alert-danger"><small>Please Check Username or Password</small></div>';
-                    }
+		      echo'<div class="alert alert-dismissable alert-danger"><small>'. validation_errors().'</small></div>';
+            }else{
+                $name = $this->input->post('username');
+                $password = $this->input->post('password');
+                if($this->Auth_model->auth($name, $password)){
+                }else{
+                    echo'<div class="alert alert-dismissable alert-danger"><small>Please Check Username or Password</small></div>';
+                }
             }
         }
         
-       public function logout()
-        {
-        $this->session->sess_destroy();
-        delete_cookie();
+       /**
+        * Admin::logout()
+        * 
+        * @return
+        */
+       public function logout(){
+            $this->session->sess_destroy();
+            delete_cookie();
             redirect('admin/' ,'refresh');
             exit;
         }
-       public function sign_in()
-        {
-         $this->load->view('admin/view_login');   
+       /**
+        * Admin::sign_in()
+        * 
+        * @return
+        */
+        public function sign_in(){
+            $this->load->view('admin/view_login');   
         }
-       public function sign_up()
+       /**
+        * Admin::sign_up()
+        * 
+        * @return
+        */
+        public function sign_up()
         {
-          $this->load->view('view_register');  
+            $this->load->view('view_register');  
         }
-        
-       
-        
-            
 }
 
 /* End of file welcome.php */
